07372ef395934ffd9ce7bc1e87e774a8a9088de5
RMKendall <ryan.morris.kendall@gmail.com>
2020-11-26 11:55:29 -0600

    Complete Step 1 of Course Content

---
+++ spring-boot-first-app/src/test/java/com/in28minutes/springboot/web/springbootfirstapp/SpringBootFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.web.springbootfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
